d6095573cd01294c1a00a82c33cd12f4b2b5c8b1
Michael Heeter <137200885+CheaTeR13@users.noreply.github.com>
2023-11-03 15:00:06 -0400

    Restful

--- 3-express/express.js
+++ 3-express/express.js
@@ -14,7 +14,13 @@ const getPetsObj = async () => {
     return JSON.parse(petData)
 }
 
-app.use(express.json())
+app.use((err,req,res,next) => {
+    if(err){
+        res.status(500).send('Internal Server Error')
+    }else{
+        next()
+    }
+})
 
 app.get('/pets', async (req,res) => {
     try{
@@ -27,18 +33,28 @@ app.get('/pets', async (req,res) => {
 
 app.get('/pets/:id', async (req,res) => {
     try{
-        const index = parseInt(req.params.id, 10)
+        const pets = await getPetsObj()
+        aName = req.params.id
+        const index = parseInt(aName, 10)
+        console.log(index)
+        const result = pets.filter(obj => Object.values(obj).includes(aName, 10))
+        console.log(result)
         if (isNaN(index)){
-            return res.status(400).send('Bad Request: Invalid pet index')
+            if (req.params.id === result){
+                return res.status(200).json(result)
+            }else{
+                console.log('err')
+                return res.status(400).send('Bad Request: Invalid pet index')
+            }
         }
 
-        const pets = await getPetsObj()
         if(index >= pets.length || index < 0){
             res.status(404).send('Not Found')
         }else{
             res.status(200).json(pets[index])
         }
     }catch(error){
+        console.log(error)
         console.log('end')
         res.status(500).send('Internal Server Error')
     }
